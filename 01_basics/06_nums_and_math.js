9f0a202d7ddb5d64913bd9025bfa4332416cbcb6
MusawirRehman <musawirrehman0808@gmail.com>
2025-05-09 18:31:29 +0000

    06_nums_and_math.js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+// let myyoutubename = "mussidotcom"
+
+// let anothername = myyoutubename
+// anothername = "chaiaurcode"
+
+// console.log( myyoutubename);
+// console.log(anothername);
+
+// let userOne ={
+//     email:"google.com",
+//     upiId:"user@upi"
+// }
+// let userTwo = userOne
+// userTwo.email = "musssi@gmail.com"
+// console.log(userOne.email);
+// console.log(userTwo.email);
+// userOne and userTwo are pointing to the same object in memory
+// so if we change the value of userTwo, it will also change the value of userOne
+// to avoid this we can use spread operator
+
+// ****************Maths********************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random());
+console.log(Math.random() * 10) ;
+console.log((Math.random() * 10) + 1);
+
+
+
